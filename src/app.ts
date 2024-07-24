6d89f1c973cbed9f56a539a03d6291ca14a7882b
helena <heju1958@outlook.com>
2024-07-24 16:52:37 -0300

    feat:cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
-import "reflect-metadata";
-import express from "express";
 import "express-async-errors";
+import "reflect-metadata";
+
+import cors from "cors";
+import express from "express";
 import handleErrorMiddleware from "./middlewares/handleError.middlewares";
 import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "./swagger/swaggerOptions";
@@ -9,6 +11,13 @@ import userRoutes from "./routers/users.routes";
 import loginRoutes from "./routers/login.routes";
 
 const app = express();
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 
 app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
